e540b8f1474a7b8d388c0f80bea08dc240b7515d
Nicole Bradford <nbradf2@gmail.com>
2018-01-18 18:10:11 -0600

    add test for game.test.js to render new game

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -7,5 +7,17 @@ describe('<Game />', () => {
 	it('Renders without crashing', () => {
 		shallow(<Game />);
 	});
-	// 
+	it('Renders new game', () => {
+		const wrapper = shallow(<Game />);
+		wrapper.setState({
+			guesses: [4, 29, 98],
+			feedback: 'Good!',
+			correctAnswer: -1
+		});
+		wrapper.instance().restartGame();
+		expect(wrapper.state('guesses')).toEqual([]);
+		expect(wrapper.state('feedback')).toEqual('Make your guess!');
+		expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
+		expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+	});
 })
