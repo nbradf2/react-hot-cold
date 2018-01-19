3674dce8d65309843af21c5807da609885e3cbfe
Nicole Bradford <nbradf2@gmail.com>
2018-01-18 18:32:30 -0600

    add test to render onMakeGuess on submit

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import GuessForm from './guess-form';
 
 describe('<GuessForm />', () => {
 	it('Renders without crashing', () => {
 		shallow(<GuessForm />);
-	})
+	});
 
-	it('Submits guess on click', () => {
-
-	})
+	it('Renders onMakeGuess on submit', () => {
+		const callback = jest.fn();
+		const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+			const value = 10;
+			wrapper.find('input[type="number"]').instance().value = value;
+			wrapper.simulate('submit');
+			expect(callback).toHaveBeenCalledWith(value.toString());
+	});
 })
