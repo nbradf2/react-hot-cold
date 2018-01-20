ead2a4cb1390db2fd583220fda4c07054824a2fe
Nicole Bradford <nbradf2@gmail.com>
2018-01-19 18:30:10 -0600

    add actions to actions.js

---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+import {RESTART_GAME, MAKE_GUESS} from './actions';
+
+const initialState = {
+	guesses: [],
+	feedback: 'Make your guess!',
+	correctAnswer: Math.round(Math.random() *100) + 1
+};
+
+export default (state = initialState, action) => {
+	if (action.type === RESTART_GAME) {
+		return Object.assign({}, state, {
+			guesses: [],
+			feedback: 'Make your guess!',
+			correctAnswer: action.correctAnswer
+		});
+	}
+
+	if (action.type === MAKE_GUESS) {
+		let feedback, guess;
+
+		guess = parseInt(action.guess, 10);
+		if (isNaN(guess)) {
+			feedback = 'Please enter a valid number.';
+
+			return Object.assign({}, state, {
+				feedback,
+				guesses: [...state.guesses, guess]
+			});
+		}
+
+		const difference = Math.abs(guess - state.correctAnswer);
+
+		if (difference >= 50) {
+			feedback = "You're Ice Cold...";
+		} else if (difference >= 30) {
+			feedback = "You're Cold...";
+		}
+	}
+
+	return state;
+}
+
